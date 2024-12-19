f2c4c00d2f28515752536cb00aad6cb287fda359
Icaro Xenofonte <54599324+xenofontee@users.noreply.github.com>
2024-12-19 08:07:55 -0300

    created vercel file

--- .next/static/webpack/webpack.71f0cd091cebef7b.hot-update.js
+++ .next/static/webpack/webpack.71f0cd091cebef7b.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "d4c384a2a77fa02c"; }
+/******/ 	__webpack_require__.h = function() { return "61ba9a9c72acae3c"; }
 /******/ }();
 /******/ 
 /******/ }
