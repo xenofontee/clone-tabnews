f6453c28b4500b6e392cbda7c19c4508f2ff8924
Icaro Xenofonte <54599324+xenofontee@users.noreply.github.com>
2024-12-22 19:13:26 -0300

    adiciona scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 aprendendo do zero
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home () {
-    return <h1> Teste 3 </h1>
+function Home() {
+  return <h1> Teste 3 </h1>;
 }
 
-export default Home
+export default Home;
